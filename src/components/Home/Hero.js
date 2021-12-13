061fdfc2745cc7a4c13da18271a454b5704c14fd
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-12-14 00:15:30 +0100

    hero

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -5,9 +5,15 @@ function Hero() {
     <div className='hero'>
       <div className='hero__content'>
         <h3 className='hero-header--third'>New Product</h3>
-        <h1 className='hero-header--first margin-top-medium'>XX99 Mark II HEADPHONES</h1>
-        <p className='hero-text margin-top-medium'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiasist.</p>
-        <button className='button margin-top-medium'>See Product</button>
+        <h1 className='hero-header--first margin-top-small'>XX99 Mark II HEADPHONES</h1>
+        <p className='hero-text margin-top-small'>
+          Experience natural, lifelike audio and exceptional
+          <br></br>
+          build quality made for the passionate music
+          <br></br>
+          enthusiasist.
+        </p>
+        <button className='btnOrange margin-top-small'>See Product</button>
       </div>
     </div>
   )
