95265ad8b7344c9b5116ceed5cb2d969d9832b9f
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-12-13 03:30:08 +0100

    hero

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -3,7 +3,12 @@ import React from 'react'
 function Hero() {
   return (
     <div className='hero'>
-      <button className='button'>See Product</button>
+      <div className='hero__content'>
+        <h3 className='hero-header--third'>New Product</h3>
+        <h1 className='hero-header--first margin-top-medium'>XX99 Mark II HEADPHONES</h1>
+        <p className='hero-text margin-top-medium'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiasist.</p>
+        <button className='button margin-top-medium'>See Product</button>
+      </div>
     </div>
   )
 }
