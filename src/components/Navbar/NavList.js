e69f436ccfbf0e6b4414f6185d4adee81a4b3105
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-12-13 02:48:11 +0100

    navlist

---
+++ src/components/Navbar/NavList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavList() {
+  return (
+    <ul className='navlist'>
+      <li className='navlist-item'>Home</li>
+      <li className='navlist-item'>Headphones</li>
+      <li className='navlist-item'>Speakers</li>
+      <li className='navlist-item'>Earphones</li>
+    </ul>
+  )
+}
+
+export default NavList
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,12 @@
 import React from 'react'
+import NavList from './NavList'
 
 function Navbar() {
   return (
     <div className='navbar'>
       <nav className='navbar__elements'>
         <img className='navbar__elements-logo' alt='AUDIOPHILE LOGO' />
-        <ul className='navbar__elements-list'>
-          <li className='navbar_elements-list--item'>Home</li>
-          <li className='navbar_elements-list--item'>Headphones</li>
-          <li className='navbar_elements-list--item'>Speakers</li>
-          <li className='navbar_elements-list--item'>Earphones</li>
-        </ul>
+        <NavList />
         <img className='navbar__elements-cart' alt='AUDIOPHILE CART ICON' />
       </nav>
     </div>
