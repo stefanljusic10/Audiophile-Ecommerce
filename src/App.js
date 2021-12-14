e25ae2150dbac965226dd64db4a6c162cf68cb0d
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-12-14 23:47:57 +0100

    providing local json

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import Hero from './components/Home/Hero'
+import React, { createContext } from 'react'
+import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar'
+import store from './services/data.json'
 import './scss/main.scss'
+
+export const StoreContext = createContext()
 
 function App() {
   return (
     <div className='app'>
-      <Navbar />
-      <Hero />
+      <StoreContext.Provider value={store}>
+        <Navbar />
+        <Home />
+      </StoreContext.Provider>
     </div>
   )
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import Hero from './Hero'
+
+function Home() {
+  return (
+    <div className='home'>
+      <Hero />
+    </div>
+  )
+}
+
+export default Home
