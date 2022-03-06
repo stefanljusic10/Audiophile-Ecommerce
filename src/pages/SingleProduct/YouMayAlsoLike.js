8664c2590201c3b3177986e31355fad5ae76979a
unknown <stefanljusic10@gmail.com>
2022-03-06 23:04:52 +0100

    similar products

--- src/pages/SingleProduct/YouMayAlsoLike.js
+++ src/pages/SingleProduct/YouMayAlsoLike.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import data from '../../services/data.json'
 
 function YouMayAlsoLike() {
+  const { singleProduct } = useParams()
+  // filtering similar products to current
+  const similarProducts = data.filter((item) => item.slug === singleProduct)[0].others
+
+  const similarProductsBox = similarProducts.map((item) => (
+    <div className='similarProducts__box-item'>
+      <img className='similarProducts__box-item--image' src={item.image.desktop} alt={item.name}></img>
+      <div className='similarProducts__box-item--heading heading-mini margin-top-small'>{item.name}</div>
+      <button className='btnOrange margin-top-small'>See Product</button>
+    </div>
+  ))
+
   return (
     <div className='similarProducts margin-top-huge'>
       <div className='similarProducts__heading heading-smaller'>You May Also Like</div>
-      <div className='similarProducts__box'></div>
+      <div className='similarProducts__box margin-top-medium'>
+        {similarProductsBox}
+      </div>
     </div>
   )
 }
